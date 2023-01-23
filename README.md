51e6c99225aa27522d33e71707ca692fb49ad7d8
Bogdan <bogdangaponenko1996@gmail.com>
2023-01-23 12:02:21 -0600

    with issues

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goit-js-hw-07
+
+1. Использование data атрибутов
+   При использовании data атрибутов в html теге, мы можем с помощью JS обратиться к данному атрибуту используя свойство `dataset`. При этом мы как бы разделяем название атрибута на 2 свойства в JS первое - `dataset`, второе - `nameOfDataSetAtribute`;
+   Пример
+
+   ```html
+   <img class="img" data-source="${img.original}" alt="${img.description}" />
+   ```
+
+```js
+$ELEMENT.addEventListener("click", onCreateImgOnListItem);
+
+function onCreateImgOnListItem(evt) {
+  console.log(evt.target.dataset.source);
+}
+```
---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from "./gallery-items.js";
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const createGallery = galleryItems
+  .map((img) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>
+      `;
+  })
+  .join("");
+gallery.innerHTML = createGallery;
+
+gallery.addEventListener("click", onCreateImgOnListItem);
+
+function onCreateImgOnListItem(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const activeFullScreenImg = basicLightbox.create(
+    ` <img src="${evt.target.dataset.source}">`
+  );
+  activeFullScreenImg.show();
+}
